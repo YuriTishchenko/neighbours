c208ecfdabfe299546c2be16a6b46e3d1daf8ec8
Юрий Тищенко <yuri.tishchenko@baltinfocom.ru>
2025-04-14 18:16:55 +0300

    1

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -25,7 +25,7 @@ config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
